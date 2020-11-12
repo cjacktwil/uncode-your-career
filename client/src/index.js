3ab893608a0b710d7fb11f691337754c88be095e
Rachel Fritz <rachelfritz0@gmail.com>
2020-11-11 18:36:58 -0600

    updates to pwa

--- client/src/index.js
+++ client/src/index.js
@@ -15,8 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
